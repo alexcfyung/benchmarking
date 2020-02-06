d1c2a8ca0cc1d0f3ab1eae6f4728189f6fd691f5
ALEXY <alex.yung@ibm.com>
2020-02-05 21:19:41 -0500

    add new setup script to avoid long download time

---
+++ experimental/benchmarks/acmeair-zos/setupJmeter_Pre.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#Run on linux
+JMETER_VERSION=apache-jmeter-3.1
+DIR=`dirname $0`
+CURRENT_DIR=`cd $DIR;pwd`
+echo $DIR
+echo $CURRENT_DIR
+mkdir ${CURRENT_DIR}/nd/Jmeter_setup
+pushd ${CURRENT_DIR}/nd/Jmeter_setup
+cp ${HOME}/${JMETER_VERSION}.tgz ./
+gunzip ${JMETER_VERSION}.tgz
+tar -xf ${JMETER_VERSION}.tar
+
+git clone https://github.com/acmeair/acmeair-nodejs
+pushd acmeair-nodejs
+#git checkout 009bd063700089a2680b696336d87bd97e412f0e
+if [ -z $ACME_PORT ]; then
+    ACME_PORT=9080
+fi
+sed -i 's/9080/'"$ACME_PORT"'/g' settings.json
+chtag -tc 819 settings.json
+popd
+
+mv acmeair-nodejs ../
+mv ${JMETER_VERSION}/*  ../Jmeter
+chtag -tc 819 ../Jmeter/bin/*
+popd
+#mkdir ${CURRENT_DIR}/nd/mongo3
+#cp ${CURRENT_DIR}/mongodb.sh ${CURRENT_DIR}/nd/mongo3/
+
