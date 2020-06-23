65a07ef74976cf6518329af51fc71d7ad4fac5ff
alexcfyung <alexcfyung@hotmail.com>
2020-06-23 19:35:51 -0400

    add custom start script

---
+++ experimental/benchmarks/acmeair/start.sh
@@ -0,0 +1,43 @@
+usage() {
+    cat <<END
+$0
+
+Acmeair benchmark on z/OS
+
+Syntax:
+
+    $0 [-ic] base_node_path compare_node_path
+
+Compare Acmeair benchmark with different node version
+
+  -i    Install and setup necessary tools like Jmeter driver and acmeair repo, reinstall if existed
+  -c    Clean up all setup files and repository
+
+END
+    exit 1
+}
+
+while getopts "ic" c; do
+    case $c in
+        i) install=true ;;
+        c) cleanup=true ;;
+        *) usage ;;
+    esac
+done
+
+shift $((OPTIND-1))
+
+if [ $install ]; then
+    rm -rf ./nd
+    mkdir ./nd ./nd/Jmeter 
+    ./setupJmeter.sh
+fi
+
+if [ $cleanup ]; then
+    rm -rf ./nd
+    exit 0
+fi
+
+ln -sf $1 nd/node_baseline
+ln -sf $2 nd/node_undertest 
+./run_acmeair_docker.sh
